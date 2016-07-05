9b00499ce2afcfb46541cb58e9629cbe431d4466
Evgeniy331 <jknoxville712@gmail.com>
2016-07-05 18:16:36 +0300

    8: добавлены новые тесты функции в main

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ int main() {
 
 	displayGreetingMessage();
 
-	cout<<"2 + 3 = "<< sumOf2Numbers << endl;
+	cout<<"2 + 3 = "<< sumOf2Numbers(2,3) << endl;
+	cout<<"1 + 3 = "<< sumOf2Numbers(1,3) << endl;
+	cout<<"7 + 4 = "<< sumOf2Numbers(7,4) << endl;
 
 	return 0;
 }
